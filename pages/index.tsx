fecd12ffa6ba002b7f6e17e4ecf8d4b63b30158f
linyows <linyows@gmail.com>
2023-09-21 11:56:24 +0900

    add twitter card

--- pages/index.tsx
+++ pages/index.tsx
@@ -67,6 +67,9 @@ export default function Home({ about, coc, eve, team, events, members, facts, og
         <title>{title}</title>
         <meta name="description" content={desc} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="twitter:image" content={`${url}/${ogimage}`} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@fukuokago" />
         <meta property="og:site_name" content={title} />
         <meta property="og:description" content={desc} />
         <meta property="og:image" content={`${url}/${ogimage}`} />
