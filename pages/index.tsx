7b6d4b74960cc33a26ef3bb462b924d2f6366a0c
linyows <linyows@gmail.com>
2023-09-23 21:42:42 +0900

    fix twitter meta

--- pages/index.tsx
+++ pages/index.tsx
@@ -61,18 +61,20 @@ export default function Home({ about, coc, eve, team, events, members, facts, og
   const title = 'Fukuoka.go'
   const desc = 'A gopher community in Fukuoka'
   const url = 'https://fukuokago.dev'
+  const image = `${url}/${ogimage}`
   return (
     <>
       <Head>
         <title>{title}</title>
         <meta name="description" content={desc} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="twitter:image" content={`${url}/${ogimage}`} />
+        <meta name="twitter:image" content={image} />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@fukuokago" />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={desc} />
         <meta property="og:site_name" content={title} />
         <meta property="og:description" content={desc} />
-        <meta property="og:image" content={`${url}/${ogimage}`} />
+        <meta property="og:image" content={image} />
         <meta property="og:image:width" content="1600" />
         <meta property="og:image:height" content="630" />
         <meta property="og:url" content={url} />
